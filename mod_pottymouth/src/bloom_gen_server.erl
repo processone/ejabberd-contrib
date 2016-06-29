1b86f7ccaa5ea1b69a191b0b089eeb360685aeb3
Tom Quackenbush <tom@madglory.com>
2016-06-29 21:19:12 +0000

    mod_pottymouth: update bloom_gen_server to support arbitraty list sizes. tested up to 87M terms. update README.

--- mod_pottymouth/src/bloom_gen_server.erl
+++ mod_pottymouth/src/bloom_gen_server.erl
@@ -18,24 +18,16 @@ serverName(Lang) ->
 member({Lang, Word} = _MessageToken) ->
   gen_server:call(serverName(Lang), {member, Word}).
 
-loadWordList(BlacklistFile) ->
-  BlacklistExists = filelib:is_file(BlacklistFile),
-  if
-    BlacklistExists ->
-      {ok, S} = file:read_file(BlacklistFile);
-    true ->
-      ?ERROR_MSG("Blacklist file not found: ~p~n", [BlacklistFile]),
-      S = <<>>
-  end,
-  WordList = string:tokens(binary_to_list(S), "\n"),
-  WordList.
-
 start({Lang, BlacklistFile} = _Opts) ->
   gen_server:start_link({local, serverName(Lang)}, ?MODULE, [BlacklistFile], []).
 
+stop() ->
+  ok.
+
 init([BlacklistFile]) ->
-  WordList = loadWordList(BlacklistFile),
-  {ok, etbloom:bloom(WordList)}.
+  ?INFO_MSG("Building bloom", []),
+  Bloom = etbloom:sbf(10000000),
+  {ok, loadWordList(Bloom, BlacklistFile)}.
 
 handle_call({member, Word}, _From, Bloom) ->
   Reply = etbloom:member(Word, Bloom),
@@ -45,3 +37,19 @@ handle_cast(_Msg, State) -> {noreply, State}.
 handle_info(_Info, State) -> {noreply, State}.
 terminate(_Reason, _State) -> ok.
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+loadWordList(Bloom, BlacklistFile) ->
+  BlacklistExists = filelib:is_file(BlacklistFile),
+  if
+    BlacklistExists ->
+      {ok, S} = file:open(BlacklistFile, read),
+      loadWordList(io:get_line(S, ''), Bloom, S);
+    true ->
+      ?ERROR_MSG("Blacklist file not found: ~p~n", [BlacklistFile]),
+      loadWordList(eof, Bloom, BlacklistFile)
+  end.
+
+loadWordList(eof, Bloom, _S) ->
+  Bloom;
+loadWordList(Line, Bloom, S) ->
+  loadWordList(io:get_line(S, ''), etbloom:add(lists:droplast(Line), Bloom), S).
