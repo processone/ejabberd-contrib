f32bb792bc4d5091b0d78cd3d3971b17be6fdf75
Badlop <badlop@process-one.net>
2020-01-29 11:33:55 +0100

    Update copyright to 2020 (processone/ejabberd#3149)

--- mod_deny_omemo/src/mod_deny_omemo.erl
+++ mod_deny_omemo/src/mod_deny_omemo.erl
@@ -5,7 +5,7 @@
 %%% Created : 18 Mar 2018 by Holger Weiss <holger@zedat.fu-berlin.de>
 %%%
 %%%
-%%% ejabberd, Copyright (C) 2018-2019   ProcessOne
+%%% ejabberd, Copyright (C) 2018-2020   ProcessOne
 %%%
 %%% This program is free software; you can redistribute it and/or
 %%% modify it under the terms of the GNU General Public License as
