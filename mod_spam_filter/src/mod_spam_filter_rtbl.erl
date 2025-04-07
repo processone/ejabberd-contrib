24734471c0ba096cd3f3472c29b83d0f3fe23328
Stefan Strigler <stefan@strigler.de>
2025-04-07 10:31:54 +0200

    mod_spam_filter: make rtbl_domains_node a config option

--- mod_spam_filter/src/mod_spam_filter_rtbl.erl
+++ mod_spam_filter/src/mod_spam_filter_rtbl.erl
@@ -3,49 +3,51 @@
 -include_lib("xmpp/include/xmpp.hrl").
 -include("logger.hrl").
 
--define(RTBL_DOMAINS_NODE, <<"spam_source_domains">>).
+-define(SERVICE_MODULE, mod_spam_filter).
 -define(SERVICE_JID_PREFIX, "rtbl-").
 
 -export([parse_blocked_domains/1,
 	 parse_pubsub_event/1,
 	 pubsub_event_handler/1,
-	 request_blocked_domains/2,
-	 subscribe/2,
-	 unsubscribe/2]).
+	 request_blocked_domains/3,
+	 subscribe/3,
+	 unsubscribe/3]).
 
-subscribe(RTBLHost, From) ->
+subscribe(RTBLHost, RTBLDomainsNode, From) ->
     FromJID = service_jid(From),
     SubIQ = #iq{type = set, to = jid:make(RTBLHost), from = FromJID,
 		sub_els = [
-			   #pubsub{subscribe = #ps_subscribe{jid = FromJID, node = ?RTBL_DOMAINS_NODE}}]},
+			   #pubsub{subscribe = #ps_subscribe{jid = FromJID, node = RTBLDomainsNode}}]},
     ?DEBUG("Sending subscription request:~n~p", [xmpp:encode(SubIQ)]),
     ejabberd_router:route_iq(SubIQ, subscribe_result, self()).
 
-unsubscribe(undefined, _From) ->
+-spec unsubscribe(binary() | undefined, binary(), binary()) -> ok.
+unsubscribe(undefined, _PSNode, _From) ->
     ok;
-unsubscribe(RTBLHost, From) ->
+unsubscribe(RTBLHost, RTBLDomainsNode, From) ->
     FromJID = jid:make(From),
     SubIQ = #iq{type = set, to = jid:make(RTBLHost), from = FromJID,
 		sub_els = [
-			   #pubsub{unsubscribe = #ps_unsubscribe{jid = FromJID, node = ?RTBL_DOMAINS_NODE}}]},
+			   #pubsub{unsubscribe = #ps_unsubscribe{jid = FromJID, node = RTBLDomainsNode}}]},
     ejabberd_router:route_iq(SubIQ, unsubscribe_result, self()).
 
--spec request_blocked_domains(binary(), binary()) -> ok.
-request_blocked_domains(undefined, _From) ->
+-spec request_blocked_domains(binary() | undefined, binary(), binary()) -> ok.
+request_blocked_domains(undefined, _PSNode, _From) ->
     ok;
-request_blocked_domains(RTBLHost, From) ->
+request_blocked_domains(RTBLHost, RTBLDomainsNode, From) ->
     IQ = #iq{type = get, from = jid:make(From),
 	     to = jid:make(RTBLHost),
 	     sub_els = [
-			#pubsub{items = #ps_items{node = ?RTBL_DOMAINS_NODE}}]},
+			#pubsub{items = #ps_items{node = RTBLDomainsNode}}]},
     ?DEBUG("Requesting RTBL blocked domains from ~s:~n~p", [RTBLHost, xmpp:encode(IQ)]),
     ejabberd_router:route_iq(IQ, blocked_domains, self()).
 
 -spec parse_blocked_domains(stanza()) -> #{binary() => any()} | undefined.
-parse_blocked_domains(#iq{from = From, type = result} = IQ) ->
-    ?DEBUG("parsing fetched items from ~p", [From]),
+parse_blocked_domains(#iq{to = #jid{lserver = LServer}, type = result} = IQ) ->
+    ?DEBUG("parsing iq-result items: ~p", [IQ]),
+    RTBLDomainsNode = gen_mod:get_module_opt(LServer, ?SERVICE_MODULE, rtbl_domains_node),
     case xmpp:get_subtag(IQ, #pubsub{}) of
-	#pubsub{items = #ps_items{node = ?RTBL_DOMAINS_NODE, items = Items}} ->
+	#pubsub{items = #ps_items{node = RTBLDomainsNode, items = Items}} ->
 	    ?DEBUG("Got items:~n~p", [Items]),
 	    parse_items(Items);
 	_ ->
@@ -53,12 +55,13 @@ parse_blocked_domains(#iq{from = From, type = result} = IQ) ->
     end.
 
 -spec parse_pubsub_event(stanza()) -> #{binary() => any()}.
-parse_pubsub_event(Msg) ->
+parse_pubsub_event(#message{to = #jid{lserver = LServer}} = Msg) ->
+    RTBLDomainsNode = gen_mod:get_module_opt(LServer, ?SERVICE_MODULE, rtbl_domains_node),    
     case xmpp:get_subtag(Msg, #ps_event{}) of
-	#ps_event{items = #ps_items{node = ?RTBL_DOMAINS_NODE, retract = ID}} when ID /= undefined ->
+	#ps_event{items = #ps_items{node = RTBLDomainsNode, retract = ID}} when ID /= undefined ->
 	    ?DEBUG("Got item to retract:~n~p", [ID]),
 	    #{ID => false};
-	#ps_event{items = #ps_items{node = ?RTBL_DOMAINS_NODE, items = Items}} ->
+	#ps_event{items = #ps_items{node = RTBLDomainsNode, items = Items}} ->
 	    ?DEBUG("Got items:~n~p", [Items]),
 	    parse_items(Items);
 	Other ->
@@ -86,13 +89,12 @@ service_jid(Host) ->
 pubsub_event_handler(#message{from = FromJid,
 			      to = #jid{lserver = LServer,
 					lresource = <<?SERVICE_JID_PREFIX, _/binary>>}} = Msg) ->
-
-    ?DEBUG("Got RTBL message to:~n~p", [Msg]),
+    ?DEBUG("Got RTBL message:~n~p", [Msg]),
     From = jid:encode(FromJid),
-    case gen_mod:get_module_opt(LServer, mod_spam_filter, rtbl_host) of
+    case gen_mod:get_module_opt(LServer, ?SERVICE_MODULE, rtbl_host) of
 	From ->
 	    ParsedItems = parse_pubsub_event(Msg),
-	    Proc = gen_mod:get_module_proc(LServer, mod_spam_filter),
+	    Proc = gen_mod:get_module_proc(LServer, ?SERVICE_MODULE),
 	    gen_server:cast(Proc, {update_blocked_domains, ParsedItems}),
 	    %% FIXME what's the difference between `{drop, ...}` and `{stop, {drop, ...}}`?
 	    drop;
